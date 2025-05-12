f95d73fa2fcbc91c57d3cab4d6fefb0a3b431633
Matías Trovant <mtrovant@gmail.com>
2025-05-12 11:21:26 -0300

    Initial commit with orm conection and user module

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+export enum Role {
+  Admin = 'Admin',
+  Moderator = 'Moderator',
+  Player = 'Player',
+}
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, enum: Role })
+  role: Role;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  profilePicture: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop({ default: false })
+  isVerified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
