14bd914b9c79e2315047b1bf34fc4b33c3ea3a43
Matías Trovant <mtrovant@gmail.com>
2025-06-10 16:27:49 -0300

    add shema for team stats

--- Backend/src/teams/entities/team.entity.ts
+++ Backend/src/teams/entities/team.entity.ts
@@ -5,6 +5,24 @@ import { User, UserDocument } from '../../users/entities/user.entity';
 export type TeamDocument = Team & Document;
 
 @Schema()
+class TeamStats {
+  @Prop({ default: 0 })
+  goalsFor: number;
+
+  @Prop({ default: 0 })
+  goalsAgainst: number;
+
+  @Prop({ default: 0 })
+  wins: number;
+
+  @Prop({ default: 0 })
+  losses: number;
+
+  @Prop({ default: 0 })
+  draws: number;
+}
+
+@Schema()
 export class Team extends Document {
   @Prop({ required: true })
   name: string;
@@ -23,6 +41,9 @@ export class Team extends Document {
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'User' }] })
   players: (User | UserDocument | Types.ObjectId)[];
+
+  @Prop({ type: TeamStats, default: () => ({}) })
+  stats: TeamStats;
 }
 
 export const TeamSchema = SchemaFactory.createForClass(Team);
