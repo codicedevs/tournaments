af18d7775ec1f4ab5d9635b17e4cc19cfc994eac
rtrovant <ramiro.trovant@prowidesoftware.com>
2025-08-03 20:19:07 -0300

    Add SSL

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,9 +5,39 @@ import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+import * as fs from "fs";
+/**
+ * Obtiene la configuración del protocolo del servidor, http o https según si encuentra los certificados o no.
+ * @returns Devuelve los parámetros para la configuración del protocolo
+ */
+export function getProtocolConfig() {
+  let key: string | Buffer = '';
+  let cert: string | Buffer = '';
+  let protocol: 'http' | 'https' | undefined;
+  if (process.env.SSL_PRIVATE_KEY && process.env.SSL_CERT)
+    try {
+      key = fs.readFileSync(process.env.SSL_PRIVATE_KEY);
+      cert = fs.readFileSync(process.env.SSL_CERT);
+      protocol = 'https';
+    } catch (error) {
+      protocol = undefined;
+      console.error(error);
+    }
+  if (!protocol) {
+    key = '';
+    cert = '';
+    protocol = 'http';
+  }
+  return { key, cert, protocol };
+}
 
+const { key, cert, protocol } = getProtocolConfig();
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    protocol == 'https' ? { httpsOptions: { key, cert } } : undefined,
+  );
   // Enable CORS
   app.enableCors();
 
