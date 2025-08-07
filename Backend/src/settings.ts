ee1b0d3d352cd30f9aef19cfccea7676cb49a63b
Matías Trovant <mtrovant@gmail.com>
2025-08-07 17:08:29 -0300

    cambio para la base de datos

--- Backend/src/settings.ts
+++ Backend/src/settings.ts
@@ -4,4 +4,6 @@ dotenv.config();
 export const serverSetting = Object.freeze({
   PORT: +(process.env.PORT ?? 6969),
   DBPASSWORD: process.env.DBPASSWORD,
+  DB_URL: process.env.DB_URL,
+  DB_URL_TEST: process.env.DB_URL_TEST,
 });
